61911885ebb0dc9e1a465372a5fe98c650874084
namangup <c0d3rpr0@gmail.com>
2019-12-28 18:47:11 +0530

    28/12

--- Web/NodePro1/index.js
+++ Web/NodePro1/index.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const app = express();
-app.get('/a',(req,res)=>{
+const bodyParser = require('body-parser');
+const Student = require('./models/student.js');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
+
+app.post('/a',(req,res)=>{
+    //console.log(req);
+   // console.log(res);
     console.log('Called');
-    res.send('Hello');
+    console.log(req.body);
+    res.send({
+        a:1,
+        b:2
+    });
 });
 
 app.listen(8000,()=>{
     console.log('server is up');
 });
+
+app.post('/new',async (req,res)=>{
+    const {name,rollNo}=req.body;
+    const student = new Student({
+        name:name,
+        rollNo:rollNo
+    });
+    await student.save();
+    console.log(student);
+    res.send('created new');
+});
+/*
+(req,res,next)=>{
+    const status = verifyToken(req.co...);
+    if(status===true)
+    else res.status(400).send('Invalid Token')
+}
+userInfo
+req.user=userInfo
+next();
+*/
